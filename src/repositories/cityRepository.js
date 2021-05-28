5806709d75ae8bb971a2eb9f7bfc0240cb962cd4
EmanuelCruz <emanuelcruz091@gmail.com>
2021-05-28 03:29:38 -0300

    Fix: Test Error

--- src/repositories/cityRepository.js
+++ src/repositories/cityRepository.js
@@ -11,7 +11,6 @@ class CityRepository {
     }
 
     async findCities(city) {
-        throw new Error("Error de prueba");
         try {
             const instance = axios.create({
                 baseURL: `${this.pathBase}${city}.json`,
